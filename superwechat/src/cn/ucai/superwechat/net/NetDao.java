ea29a3b7bb1703c956eb3d817a6244527f0ae5e8
wangxinyuan <1735267615@qq.com>
2017-02-08 22:08:53 +0800

    修改闪屏界面，添加欢迎界面，添加butterknife及依赖包,添加网络工具类，添加跳转工具类，添加I接口，修改注册界面，设置环形appkey，添加实体类，实现用户注册，

---
+++ superwechat/src/cn/ucai/superwechat/net/NetDao.java
@@ -0,0 +1,35 @@
+package cn.ucai.superwechat.net;
+
+import android.content.Context;
+
+import cn.ucai.superwechat.I;
+import cn.ucai.superwechat.utils.MD5;
+import cn.ucai.superwechat.utils.OkHttpUtils;
+
+/**
+ * Created by Administrator on 2017/2/8.
+ */
+
+public class NetDao {
+    public static void register(Context context,String username,
+                                String nick,String passwordg,OnCompleteListener<String>listener){
+        OkHttpUtils<String> utils=new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_REGISTER)
+                .addParam(I.User.USER_NAME,username)
+                .addParam(I.User.NICK,nick)
+                .addParam(I.User.PASSWORD, MD5.getMessageDigest(passwordg))
+                .post()
+                .targetClass(String.class)
+                .execute(listener);
+
+    }
+    public static void unRegister(Context context,String username,
+                             OnCompleteListener<String>listener){
+        OkHttpUtils<String> utils=new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_REGISTER)
+                .addParam(I.User.USER_NAME,username)
+                .targetClass(String.class)
+                .execute(listener);
+
+    }
+}
---
+++ superwechat/src/cn/ucai/superwechat/net/OnCompleteListener.java
@@ -0,0 +1,11 @@
+package cn.ucai.superwechat.net;
+
+
+import cn.ucai.superwechat.utils.OkHttpUtils;
+
+/**
+ * Created by Administrator on 2017/1/11 0011.
+ */
+
+public interface OnCompleteListener<T> extends OkHttpUtils.OnCompleteListener<T>{
+}
