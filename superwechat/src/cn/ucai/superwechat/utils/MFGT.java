6de0d2dab25073b65695f1ea2ce7c5d3b8018053
wangxinyuan <1735267615@qq.com>
2017-02-12 17:45:55 +0800

    重构Settings界面

--- superwechat/src/cn/ucai/superwechat/utils/MFGT.java
+++ superwechat/src/cn/ucai/superwechat/utils/MFGT.java
@@ -2,11 +2,13 @@ package cn.ucai.superwechat.utils;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
 
 import cn.ucai.superwechat.R;
 import cn.ucai.superwechat.ui.GuideActivity;
 import cn.ucai.superwechat.ui.LoginActivity;
 import cn.ucai.superwechat.ui.RegisterActivity;
+import cn.ucai.superwechat.ui.SettingsActivity;
 import cn.ucai.superwechat.ui.SplashActivity;
 
 
@@ -40,5 +42,9 @@ public class MFGT {
 
     public static void gotoGuide(SplashActivity activity) {
         startActivity(activity, GuideActivity.class);
+    }
+
+    public static void gotoSettings(FragmentActivity activity) {
+        startActivity(activity, SettingsActivity.class);
     }
 }
