8f80136348262ecb6fb9c23467143fba09fbb773
wangxinyuan <1735267615@qq.com>
2017-02-13 18:24:50 +0800

    修改添加里的搜索好友功能

--- superwechat/src/cn/ucai/superwechat/utils/MFGT.java
+++ superwechat/src/cn/ucai/superwechat/utils/MFGT.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 
+import com.hyphenate.easeui.domain.User;
+
 import cn.ucai.superwechat.R;
 import cn.ucai.superwechat.ui.AddContactActivity;
 import cn.ucai.superwechat.ui.GuideActivity;
@@ -59,5 +61,8 @@ public class MFGT {
     }
     public static void gotoAddContact(Activity activity){
         startActivity(activity, AddContactActivity.class);
+    }
+
+    public static void gotoFirent(Activity activity, User user) {
     }
 }
