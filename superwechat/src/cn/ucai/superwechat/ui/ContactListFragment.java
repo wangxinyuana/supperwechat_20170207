2436eb50c215bb19d75f2d5e2a765f254d9d6cdb
wangxinyuan <1735267615@qq.com>
2017-02-16 21:00:23 +0800

    删除联系人

--- superwechat/src/cn/ucai/superwechat/ui/ContactListFragment.java
+++ superwechat/src/cn/ucai/superwechat/ui/ContactListFragment.java
@@ -41,7 +41,11 @@ import cn.ucai.superwechat.SuperWeChatHelper;
 import cn.ucai.superwechat.SuperWeChatHelper.DataSyncListener;
 import cn.ucai.superwechat.db.InviteMessgeDao;
 import cn.ucai.superwechat.db.UserDao;
+import cn.ucai.superwechat.domain.Result;
+import cn.ucai.superwechat.net.NetDao;
+import cn.ucai.superwechat.net.OnCompleteListener;
 import cn.ucai.superwechat.utils.MFGT;
+import cn.ucai.superwechat.utils.ResultUtils;
 import cn.ucai.superwechat.widget.ContactItemView;
 
 /**
@@ -214,9 +218,6 @@ public class ContactListFragment extends EaseContactListFragment {
                 e.printStackTrace();
             }
             return true;
-        }else if(item.getItemId() == R.id.add_to_blacklist){
-            moveToBlacklist(toBeProcessUsername);
-            return true;
         }
         return super.onContextItemSelected(item);
     }
@@ -234,6 +235,34 @@ public class ContactListFragment extends EaseContactListFragment {
         pd.setMessage(st1);
         pd.setCanceledOnTouchOutside(false);
         pd.show();
+        NetDao.removeContact(getContext(), EMClient.getInstance().getCurrentUser(),
+                tobeDeleteUser.getMUserName(),
+                new OnCompleteListener<String>() {
+                    @Override
+                    public void onSuccess(String s) {
+                        if (s!=null){
+                            Result result = ResultUtils.getListResultFromJson(s, User.class);
+                            if (result!=null&&result.isRetMsg()){
+                                // remove user from memory and database
+                                UserDao dao = new UserDao(getActivity());
+                                dao.deleteAppContact(tobeDeleteUser.getMUserName());
+                                SuperWeChatHelper.getInstance().getAppContactList().remove(tobeDeleteUser.getMUserName());
+                                getActivity().runOnUiThread(new Runnable() {
+                                    public void run() {
+                                        pd.dismiss();
+                                        contactList.remove(tobeDeleteUser);
+                                        contactListLayout.refresh();
+                                    }
+                                });
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onError(String error) {
+
+                    }
+                });
         new Thread(new Runnable() {
             public void run() {
                 try {
